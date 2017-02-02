02e374973e9915f730f5f84c55677446e2b1377b
alexhaynes32 <alhaynes@seas.upenn.edu>
2017-02-02 16:15:57 -0500

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Alex Haynes on 2/2/17.
+//  Copyright © 2017 Alex Haynes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
